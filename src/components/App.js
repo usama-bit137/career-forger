e943bc3868513684d05a43436004553c86666220
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-11 14:07:55 +0100

    Form default state moved to App.js and state passed to Form and Render as props. This allows the app to render the CV in real-time. App.css contains styles for the Form and Render to be displayed next to each other.

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,38 @@ import React, { Component } from "react"
 import '../styles/App.css';
 import Header from './Header'
 import Form from "./Form"
+import Render from "./Render";
 import Footer from "./Footer"
 
+const formState = require("../cvState.json")
+
 export default class App extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
+
+    this.state = formState;  
   }
   
+  handleChange = (e) => {
+    this.setState({
+        ...this.state, 
+        [e.target.name]: [e.target.value]
+    })
+  }
+
   render() {
     return (
       <>
         <Header />
-        <Form />
+        <div className="container">
+          <Form 
+            item={this.state}
+            handleChange={this.handleChange}
+          />
+          <Render
+            item={this.state}
+          />
+        </div>
         <Footer />
       </>
     )
