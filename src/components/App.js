81c18e37a272bb3a64a8e172fb5f729ab267e6c9
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-19 22:07:58 +0100

    conditional check of parent container of target node allows us to access the 'education' and 'work' property in cvState object. Object and array decomposition was used to acheive this.

--- src/components/App.js
+++ src/components/App.js
@@ -4,34 +4,50 @@ import Header from './Header'
 import Form from "./Form"
 import Render from "./Render";
 import Footer from "./Footer"
-
-const formState = require("../cvState.json")
+import cvState from "../cvState.js"
 
 export default class App extends Component {
   constructor() {
     super()
 
-    this.state = formState;  
+    this.state = cvState;  
   }
   
   handleChange = (e) => {
-    this.setState({
-        ...this.state, 
-        [e.target.name]: [e.target.value]
-    })
+    const {name, value, parentElement} = e.target, 
+          parentName = parentElement.name
+    
+    if (parentName === "education" || "work"){
+      this.setState({
+        ...this.state,
+        [parentName]: {
+          ...this.state[parentName],
+          [name]: value
+        } 
+      })
+    } else if (parentName === "personal") {
+      this.setState({
+          ...this.state, 
+          [name]: value
+      })
+    }
+    console.log(this.state)
   }
 
   render() {
     return (
       <>
         <Header />
+        
         <div className="container">
           <Form 
-            item={this.state}
+            personal={this.state.personal}
+            education={this.state.education}
             handleChange={this.handleChange}
           />
           <Render
-            item={this.state}
+            personal={this.state.personal}
+            education={this.state.education}
           />
         </div>
         <Footer />
@@ -39,5 +55,3 @@ export default class App extends Component {
     )
   }
 }
-
-
