6861a61a64d271fe08d907f141d96b2a0b093056
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-19 22:14:24 +0100

    Styles added to render. Also. Correctly wired up the change handler to the Render component so that editting hte form will change the correct segments of the CV render.

--- src/components/Render.js
+++ src/components/Render.js
@@ -5,29 +5,30 @@ export default class Render extends Component {
     constructor(props) {
         super(props)
 
-        this.state = this.props.item
+        this.state = this.props
     }
 
     render() {
         return (
             <section className="cv-render">
-                <h1 className="first-name">{this.props.item.firstName} {this.props.item.lastName}</h1>
+                <h1 className="name">{this.props.personal.firstName} {this.props.personal.lastName}</h1>
                 <span>
                     <h3 className="phone">
-                        Phone: {this.props.item.phone} 
+                        Phone: {this.props.personal.phone} 
                     </h3>
                     <h3> 
-                        Email: {this.props.item.email}
+                        Email: {this.props.personal.email}
                     </h3>
                 </span>
-                <div>
-                    Profile
+                <div className="description">
+                    <h3>Profile</h3>
+                    {this.props.personal.profile}
                 </div>
 
                 <h3>Education</h3>
-                <ul>
-                    <li></li>
-                </ul>
+                    <h4>{this.props.education.institution} <span className="education-major">{this.props.education.major}</span> </h4> 
+                    <div className="dates">{this.props.education.start} - {this.props.education.end} </div>
+                    <div className="description">{this.props.education.description}</div>
                 <h3>Work Experience</h3>
                 <ul>
                     <li></li>
