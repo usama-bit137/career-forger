9c58100bce75378bc32f9049b12b7fc254cab9d6
usama-bit137 <usamaaqeel98@gmail.com>
2023-05-19 22:03:59 +0100

    Initial for state exported from JavaScript Object. cvState altered as nested array/object indexing was extremely complicated.

---
+++ src/cvState.js
@@ -0,0 +1,32 @@
+let cvState = {
+    "personal" : {
+        "firstName": "Jim Bob", 
+        "lastName": "Sherman", 
+        "phone": "+44 7659 903428", 
+        "email": "jimbob.sherman@email.com", 
+        "profile": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis rutrum dui ac venenatis vulputate. In ornare pellentesque ante, ac ultrices lorem semper vitae. Sed hendrerit, elit quis eleifend finibus, est magna viverra risus, nec pulvinar ligula leo quis tellus. Pellentesque facilisis nulla ac velit sagittis ullamcorper."
+    },
+    "education": {
+        "institution": "University College Plebdon", 
+        "major": "Plebiology with Plebiometry",
+        "start": "20/05/2004", 
+        "end" : "20/05/2007",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis rutrum dui ac venenatis vulputate. In ornare pellentesque ante, ac ultrices lorem semper vitae. Sed hendrerit, elit quis eleifend finibus, est magna viverra risus, nec pulvinar ligula leo quis tellus. Pellentesque facilisis nulla ac velit sagittis ullamcorper. "
+    },
+    "work": { 
+        "company": "", 
+        "jobTitle":"",
+        "start": "", 
+        "end" : "",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis rutrum dui ac venenatis vulputate. In ornare pellentesque ante, ac ultrices lorem semper vitae. Sed hendrerit, elit quis eleifend finibus, est magna viverra risus, nec pulvinar ligula leo quis tellus. Pellentesque facilisis nulla ac velit sagittis ullamcorper."
+    },
+    "skills": ["",""],
+    "references":{
+        "refName": "", 
+        "refType": "radio button",
+        "institution": "", 
+        "phone": "", 
+        "email": ""
+    }
+}
+module.exports = cvState;
