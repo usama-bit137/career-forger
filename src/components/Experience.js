88ec33463d1c91f4042d3c1548e4122141b139f6
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-13 15:26:42 +0100

    No major results acheived from debugging the Experience component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
 
+/* REMEMBER: This is a form element!!! */
+
 export default class Experience extends Component {
     constructor(props) {
         super(props);
@@ -7,50 +9,50 @@ export default class Experience extends Component {
     }
 
     render() {
-        return (            
-            <form className={this.props.parentName} name={this.props.parentName}>
-                <input
-                    type="text"
-                    name={this.props.place}
-                    value={this.props.item[this.props.place]} 
-                    placeholder={this.props.place}
-                    onChange={this.handleChange}
-                />
+        return ( 
+            <form className={`${this.props.parentName}-form`} name={this.props.parentName}>
+            <input
+                type="text"
+                name={this.props.place}
+                value={this.props.item[this.props.place]} 
+                placeholder={this.props.place}
+                onChange={this.handleChange}
+            />
+            <input 
+                type = "text"
+                name = {this.props.what}
+                value = {this.props.item[this.props.what]} 
+                placeholder = {this.props.what}
+                onChange = {this.handleChange}
+            />
+            
+            <label className="date-set">
+                <p className="date-title">From</p> 
                 <input 
-                    type = "text"
-                    name = {this.props.what}
-                    value = {this.props.item[this.props.what]} 
-                    placeholder = {this.props.what}
-                    onChange = {this.handleChange}
-                />
-                
-                <label>
-                    From 
-                    <input 
-                        type="date"
-                        name="start"
-                        value={this.props.item.start} 
-                        onChange={this.handleChange}
-                    />
-                </label>
-
-                <label>
-                    Until 
-                    <input 
-                        type="date"
-                        name="start"
-                        value={this.props.item.end} 
-                        onChange={this.handleChange}
-                    />
-                </label>
-                
-                <textarea 
-                    placeholder="Description"
-                    value={this.props.item.description}
-                    name="description"
+                    type="date"
+                    name="start"
+                    value={this.props.item.start} 
                     onChange={this.handleChange}
                 />
-            </form>
+            </label>
+
+            <label className="date-set">
+                <p className="date-title">Until</p> 
+                <input 
+                    type="date"
+                    name="start"
+                    value={this.props.item.end} 
+                    onChange={this.handleChange}
+                />
+            </label>
+            
+            <textarea 
+                placeholder="Description"
+                value={this.props.item.description}
+                name="description"
+                onChange={this.handleChange}
+            />
+        </form>
         )
     }
 }
