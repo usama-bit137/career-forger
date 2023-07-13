43e8d0f9d4885fd04f937a565840c2b236391cc3
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-13 15:28:48 +0100

    Form converted to functional react component. Passed the isSubmitted prop, because sumbission of the form changes the isSubmitted state from true to false.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,81 +1,88 @@
-import React, { Component } from "react"
+import React from "react";
 import Experience from "./Experience";
-import "../styles/Form.css"
+import "../styles/Form.css";
 
-export default class Form extends Component {
-    constructor(props) {
-        super(props)
-        this.handleChange = this.props.handleChange.bind(this); 
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-    
-    handleSubmit(event) {
-        event.preventDefault()
-        console.log("Somone tried to submit")
-    }
+export default function Form({
+  personal,
+  education,
+  handleChange,
+  isSubmitted,
+  setIsSubmitted,
+}) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Somone tried to submit");
+    setIsSubmitted((isSubmitted) => !isSubmitted);
+    console.log(isSubmitted);
+  }
 
-    render() {
-        return (
-            <form className = "form" name="form" onSubmit={this.handleSubmit}> 
-                <h1 className="call-to-action">Your Path to Professional Success Starts Here!</h1>
-                <h4 className="call-to-action">Enter your details below:</h4>
-                <p className="title">PERSONAL DETAILS</p>
-                <form className="personal-form" name="personal">
-                    <input 
-                        type="text" 
-                        placeholder="First Name" 
-                        name="firstName" 
-                        value={this.props.personal.firstName} 
-                        onChange={this.handleChange}
-                    />
-                    <input 
-                        type="text" 
-                        placeholder="Last Name" 
-                        name="lastName" 
-                        value={this.props.personal.lastName} 
-                        onChange={this.handleChange}
-                    />
-                    <input 
-                        type="text" 
-                        placeholder="Phone Number"
-                        name="phone" 
-                        value={this.props.personal.phone} 
-                        onChange={this.handleChange}
-                    />
-                    <input 
-                        type="email" 
-                        placeholder="Email"
-                        name="email" 
-                        value={this.props.personal.email} 
-                        onChange={this.handleChange}
-                    />
-                    
-                    <textarea 
-                        placeholder="Profile"
-                        value={this.props.personal.profile}
-                        name="profile"
-                        onChange={this.handleChange}
-                    />
-                </form>
-                <p className="title">EDUCATION</p>
-                <form className="education-form">
-                    <Experience 
-                        parentName = "education"
-                        place = "institution"
-                        what = "major"
-                        handleChange = {this.handleChange}
-                        item = {this.props.education}
-                    />
-                </form>
-                {/* <Experience 
-                        parentName = "work"
-                        place = "company"
-                        what = "jobTitle"
-                        handleChange = {this.handleChange}
-                        item = {this.props.work}
-                    /> */}
-                    <button>Submit Template</button>
-            </form>
-        )
-    }
+  function educationAdd() {
+    return (
+      <Experience
+        parentName="education"
+        place="institution"
+        what="major"
+        handleChange={handleChange}
+        item={education}
+        id={education[0]}
+      />
+    );
+  }
+
+  return (
+    <form className="form" name="form" onSubmit={handleSubmit}>
+      <h1 className="call-to-action">
+        Your Path to Professional Success Starts Here!
+      </h1>
+      <h4 className="call-to-action">Enter your details below:</h4>
+      <p className="title">PERSONAL DETAILS</p>
+      <form className="personal-form" name="personal">
+        <input
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          value={personal.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          value={personal.lastName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Phone Number"
+          name="phone"
+          value={personal.phone}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={personal.email}
+          onChange={handleChange}
+        />
+
+        <textarea
+          placeholder="Profile"
+          value={personal.profile}
+          name="profile"
+          onChange={handleChange}
+        />
+      </form>
+      <p className="title">EDUCATION</p>
+      <Experience
+        parentName="education"
+        place="institution"
+        what="major"
+        handleChange={handleChange}
+        item={education}
+      />
+      <button onClick={educationAdd}>Add Education</button>
+      <button className="create-button">CREATE</button>
+    </form>
+  );
 }
