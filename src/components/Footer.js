94ef72776ec99aa77cb9fd5ae7bac8597409d515
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-13 15:27:04 +0100

    Removed the github logo as not used.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,16 @@
 import React from "react";
-import "../styles/Footer.css"
-import github from "../icons/gitlogo.png"
+import "../styles/Footer.css";
+// import github from "../icons/gitlogo.png"
 
-export default function Footer () {
-    return (
-        <footer className="footer">
-            <p className="footer-text"> 
-                Created by Usama Aqeel. 
-                <img 
-                    className="git-logo"
-                    src={github} 
-                    alt="GitHib Logo" 
-                    width="15px" 
-                />
-                <a 
-                    className="git-link" 
-                    href="https://github.com/usama-bit137"
-                >
-                usama-bit137
-                </a>
-            </p>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">
+        Created by
+        <a className="git-link" href="https://github.com/usama-bit137">
+          usama-bit137
+        </a>
+      </p>
+    </footer>
+  );
 }
