fc648405a43e202d4075ec7590e8e40c172e16a2
usama-bit137 <usamaaqeel98@gmail.com>
2023-07-14 13:41:51 +0100

    Converted Header component to functional react instead of class based.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,12 @@
-import React, { Component } from "react";
-import icon from "../icons/cv-image.png"
-import "../styles/Header.css"
+import React from "react";
+import icon from "../icons/cv-image.png";
+import "../styles/Header.css";
 
-export default class Header extends Component {
-    render() {
-        return (
-            <header>
-                <img 
-                    src={icon} width="50px"
-                    alt="Career Forge Logo"
-                />
-                <section className="header--title">
-                    CareerForge
-                </section>
-            </header>
-        )
-    }
+export default function Header() {
+  return (
+    <header>
+      <img src={icon} width="50px" alt="Career Forge Logo" />
+      <section className="header--title">CareerForge</section>
+    </header>
+  );
 }
